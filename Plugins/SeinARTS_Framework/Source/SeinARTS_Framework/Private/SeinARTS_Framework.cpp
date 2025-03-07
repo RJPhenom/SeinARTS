39bfcc1b6fa021566b9c60437d523c2ca74e27e6
RJ <rj@macklem.ca>
2025-03-07 11:11:08 -0500

    Generate plugin source
    Generated plugin source code via editor.
    
    Added:
    SeinARTS_Framework.h
    SeinARTS_Framework.cpp
    SeinARTS_Framework.Build.cs
    SeinARTS_Framework.uplugin

---
+++ Plugins/SeinARTS_Framework/Source/SeinARTS_Framework/Private/SeinARTS_Framework.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SeinARTS_Framework.h"
+
+#define LOCTEXT_NAMESPACE "FSeinARTS_FrameworkModule"
+
+void FSeinARTS_FrameworkModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FSeinARTS_FrameworkModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FSeinARTS_FrameworkModule, SeinARTS_Framework)
